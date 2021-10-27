36e6b6274c69aa5f5417d570affd39a651b4bc39
Azhari Subroto <azhari.subroto@gmail.com>
2021-10-28 01:45:20 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'bobo'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', ''),
+        host: env('PLANETSCALE_DB_HOST', '127.0.0.1'),
+        port: env.int('PLANETSCALE_DB_PORT', 3306),
+        database: env('PLANETSCALE_DB_NAME', 'bobo'),
+        username: env('PLANETSCALE_DB_USERNAME', 'root'),
+        password: env('PLANETSCALE_DB_PASSWORD', ''),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
